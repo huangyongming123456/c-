e843374f5b6e7d1a3faf9b6b8697d137d165342b
HYM17580 <1481099383@qq.com>
2020-03-18 18:50:18 +0800

    计算器

---
+++ .vs/2_8 (1)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation a = new Calculation();
+            a.setmessage();
+            a.getmessage();
+        }
+    }
+}
